86ccce6ec79d7f4e006085c3f3b05ed66220b89d
amci1203 <allen.mcintoshii@gmail.com>
2016-10-02 13:27:53 -0400

    Added new wrapper and page-section classes

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -2,13 +2,14 @@ var gulp = require('gulp'),
     postCSS = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     nesting = require('postcss-nested'),
+    mixins = require('postcss-mixins'),
     postImport = require('postcss-import'),
     cssVars = require('postcss-simple-vars');
 
 gulp.task('css', function () {
     console.log('---> Filtering CSS file...');
     return gulp.src('./app/assets/css/styles.css')
-        .pipe(postCSS([postImport, cssVars, nesting, autoprefixer]))
+        .pipe(postCSS([postImport, cssVars, mixins, nesting, autoprefixer]))
         .on('error', function (err) {
             console.log('There seems to be an error with your CSS.');
             console.log(err.toString());
